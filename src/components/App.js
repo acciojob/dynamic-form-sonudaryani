d017b9e7a6435a6a3b15e01bf5e682407c54d63c
= <=>
2023-08-27 11:06:24 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,40 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
-const App = () => {
+export default function App() {
+  let [inputField, setInputField] = useState([]);
+  const handleClick = () => {
+    setInputField([...inputField, inputField.length + 1]);
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div className="App">
+      {inputField.length > 0 ? (
+        inputField.map((field) => (
+          <FormField
+            key={field}
+            field={field} // Pass the field value to the FormField component
+            inputField={inputField}
+            setInputField={setInputField}
+          />
+        ))
+      ) : (
+        <p>No fields in the form</p>
+      )}
+      <button onClick={handleClick}>Add Field</button>
     </div>
-  )
+  );
 }
 
-export default App
+function FormField({ field, inputField, setInputField }) {
+  const handleClick = () => {
+    const newInputField = inputField.filter((item) => item !== field); // Use the field value here
+    setInputField(newInputField);
+  };
+  return (
+    <div>
+      <input />
+      <button onClick={handleClick}>Delete</button>
+    </div>
+  );
+}
